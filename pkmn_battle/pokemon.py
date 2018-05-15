ba9b6d35693a06194d3cc64cdd72e17647373b9e
chekos <chekos.data@gmail.com>
2018-05-14 22:06:23 -0700

    Initial commit

---
+++ pkmn_battle/pokemon.py
@@ -0,0 +1,15 @@
+class Pokemon:
+    def __init__(self, attributes):
+        self.number = opponent_number
+        self.name = attributes['Name']
+        self.type1 = attributes['Type1']
+        self.type2 = attributes['Type2']
+        self.total = attributes['Total']
+        self.hp = attributes['HP']
+        self.attack = attributes['Attack']
+        self.defense = attributes['Defense']
+        self.special_atk = attributes['SpecialAtk']
+        self.special_def = attributes['SpecialDef']
+        self.speed = attributes['Speed']
+        self.generation = attributes['Generation']
+        self.legendary = attributes['Legendary']

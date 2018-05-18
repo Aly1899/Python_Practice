d1ba63fa8bc300490a2a8232c2607f992eb00006
chekos <chekos.data@gmail.com>
2018-05-18 14:32:47 -0700

    complete reimagination of game loop

--- pkmn_battle/pkmn_batte.py
+++ pkmn_battle/pkmn_batte.py
@@ -1,36 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 18 13:25:58 2018
+
+@author: sanchez
+"""
+
 import pandas as pd
+import time
 import random
-from pokemon import Pokemon
+import os
 
-df = pd.read_csv('pokemon first gen (no dups).csv', index_col=0, )
+from pokemon_creatures import Player, Opponent
 
-def create_opponent(number_id: int) -> object:
-    '''
-    This function creates a pokemon instance for the opponent.
-    :param number_id: A number between 1 and 150 relating to the gen 1 pokemon pokedex number.
-    :return: a Pokemon()
-    '''
+df = pd.read_csv('pokemon first gen (no dups).csv', index_col = 0,)
 
+opponent_number = random.randint(1, 150)
+player_number = int(input('Choose your Pokemon\' number: '))
 
-    opponent = Pokemon(df.loc[number_id])
-    return opponent
+os.system('cls')
 
-
-opponent_id = random.randint(1, 150)
-opponent = create_opponent(opponent_id)
-
-
+opponent = Opponent(df.loc[opponent_number], opponent_number)
+player = Player(df.loc[player_number], player_number)
 
 print(f'A wild {opponent.name} appeared!')
-print(f'Using your pokedex you find its stats. \n'
-      f'HP: {opponent.hp} \t'
-      f'Attack: {opponent.attack} \n'
-      f'Defense: {opponent.defense} \t'
-      f'Special Attack: {opponent.special_atk} \n'
-      f'Special Defense: {opponent.special_def} \t'
-      f'Speed: {opponent.speed} \n'
-      f'{opponent.name} is a {opponent.type1.lower()} type pokemon.')
-
+time.sleep(.8)
+print(f"Using your pokedex you can learn more about it.") 
+time.sleep(0.2)
+print(f"This {opponent.name}'s stats are: ")
+time.sleep(0.2)
+print(f"HP: {opponent.hp}") 
+time.sleep(0.2)
+print(f"Attack: {opponent.attack}")
+time.sleep(0.2)
+print(f"Defense: {opponent.defense}")
+time.sleep(0.2)
+print(f"Sp. Attack: {opponent.special_atk}")
+time.sleep(0.2)
+print(f"Sp. Defense: {opponent.special_def}")
+time.sleep(0.2)
+print(f"Speed: {opponent.speed}")
+time.sleep(0.2)
+print(f"and it's of {opponent.type1.lower()} type.\n")
+time.sleep(0.2)
 if opponent.legendary:
-    print(f"It's the legendary pokemon {opponent.name}!")
+    print(f"It's the legendary {opponent.name}!\n\n")
+    
+
+    
+while True:
+    time.sleep(0.5)
+    print(f"It's your turn {player.name}: ")
+    time.sleep(0.5)
+    print(f"{player.name} has {player.hp} left.\n")
+    time.sleep(0.5)
+    player.attack_opponent(opponent)
+    
+    if opponent.hp <= 0:
+        print(f"{opponent.name} was defeated!\n")
+        break
+    time.sleep(.8)
+    print(f"It's {opponent.name}'s turn.")
+    time.sleep(.8)
+    opponent.attack_player(player)
+    if player.hp <= 0:
+        print(f"Oh no! {player.name} fainted.")
+        os.system('cls')
+        break
+        
+    time.sleep(.8)
     
